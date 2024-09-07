8783771612a7e82c0d55313f33d5a1bbf3739d57
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-07 22:24:40 +0800

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,11 +7,17 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  val = {
+    email: "hello@gmail.com",
+    password: "123456",
+  };
+  // 设置初始值
   constructor() {}
 
   ngOnInit() {}
 
   login(result: NgForm, submit) {
     console.log(result.value, result.valid, submit);
+    console.log("val", this.val);
   }
 }
