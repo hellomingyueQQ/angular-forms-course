cf01a96f42662b028f74e48cef5a430853bafdda
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-07 21:55:49 +0800

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,16 +7,11 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
-  onEmailChange(change) {
-    console.log(change);
-    // 每次有值改变， ngmodel会改变valid状态,改变value，通知ngform directive
-  }
   constructor() {}
 
   ngOnInit() {}
 
   login(result: NgForm, submit) {
-    // submit如果不给类型，就是any类型，果然typescript是为了帮助编程的
     console.log(result.value, result.valid, submit);
   }
 }
