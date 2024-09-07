e00747c973dfec2aa0106dacf9940e0ba5d5f03b
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-07 15:15:24 +0800

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { NgFor } from "@angular/common";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  @ViewChild("loginForm") form: NgForm;
+  constructor() {}
 
+  ngOnInit() {}
 
-  constructor() {
+  test(result: NgForm) {
+    console.log(result.value, result.valid);
 
+    console.log(1, result);
+    console.log(2, this.form);
+    console.log(result instanceof NgForm);
 
+    // 这里打印出来的就是ngForm这个指令
   }
-
-  ngOnInit() {
-
-  }
-
 }
