84c48fd25bd0ded6d4f90348b16a982eac25c42d
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-07 23:12:37 +0800

    update

---
+++ src/app/directives/password-strength.directive.ts
@@ -0,0 +1,27 @@
+import { Directive } from "@angular/core";
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validator,
+} from "@angular/forms";
+import { createPasswordStrengthValidator } from "../validators/password-strength.validator";
+
+@Directive({
+  selector: "[passwordStrength]",
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: PasswordStrengthDirective,
+      multi: true,
+    },
+  ],
+})
+export class PasswordStrengthDirective implements Validator {
+  validate(control: AbstractControl): ValidationErrors | null {
+    return createPasswordStrengthValidator()(control);
+  }
+  registerOnValidatorChange?(fn: () => void): void {}
+}
+
+// 需要提醒angular，这是一个angular form field validator directive
