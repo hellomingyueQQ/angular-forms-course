d7639d23239fa7bb0fd9210497d4a5ab12c4cf12
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-07 22:48:42 +0800

    Update password-strength.validator.ts

--- src/app/validators/password-strength.validator.ts
+++ src/app/validators/password-strength.validator.ts
@@ -1,6 +1,18 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export function createPasswordStrengthValidator(): ValidatorFn {
-    // 没有错误返回null
-  return (control: AbstractControl): ValidationErrors | null => {};
+  // 没有错误返回null
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+    if (!value) {
+      return null;
+    }
+    // 如果没有值就返回null
+
+    const hasUpperCase = /[A-Z]+/.test(value); // +表达至少出现一次
+    const hasLowerCase = /[a-z]+/.test(value);
+    const hasNumeric = /[0-9]+/.test(value);
+    const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+    return !passwordValid ? { passwordStrength: true } : null;
+  };
 }
