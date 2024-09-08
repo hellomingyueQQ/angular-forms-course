239e0214c3763847592469d944454122d51c869a
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-08 17:37:24 +0800

    update

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -9,8 +9,29 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class CreateCourseStep2Component implements OnInit {
   form = this.fb.group({
     courseType: ["premium", Validators.required],
+    price: [
+      null,
+      [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(999),
+        Validators.pattern("[0-9]+"),
+      ],
+    ],
   });
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.form.valueChanges.subscribe((val) => {
+      const priceContrl = this.form.controls["price"];
+      if (val.courseType === "free" && priceContrl.enabled) {
+        // priceContrl.enabled:avoid having to constantly disable here this control every time that a new value is emitted.
+        priceContrl.disable({ emitEvent: false }); //妙啊
+        // make sure that this disabling of the price control does not trigger a new value=> infinite loop
+        // 一旦disable， form.value就不包含那个control的值了
+      } else if (val.courseType === "premium" && priceContrl.disabled) {
+        priceContrl.enable({ emitEvent: false });
+      }
+    });
+  }
 }
