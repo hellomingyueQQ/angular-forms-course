ea11bdb202a79d2b07b9eda9bb96e57a046f1432
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-08 19:56:44 +0800

    Update create-course-step-1.component.ts

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
 import { CoursesService } from "../../services/courses.service";
-import { Observable } from "rxjs";
 import { courseTitleValidator } from "../../validators/course-title.validator";
+import { filter } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 interface CourseCategory {
   code: string;
@@ -38,9 +39,22 @@ export class CreateCourseStep1Component implements OnInit {
   constructor(private fb: FormBuilder, private courses: CoursesService) {}
   ngOnInit() {
     this.courseCategories$ = this.courses.findCourseCategories();
+    const draft = localStorage.getItem("STEP_1");
+    if (draft) {
+      this.form.setValue(JSON.parse(draft));
+    }
+    this.form.valueChanges
+      .pipe(filter(() => this.form.valid))
+      .subscribe((val) => {
+        localStorage.setItem("STEP_1", JSON.stringify(val));
+      });
+    // 只保存valid value
+    // 存在了application的本地存储空间
+    // 提交后，要注意删除数据
   }
 
   get courseTitle() {
     return this.form.controls["title"];
   }
 }
+// 这节课要把用户临时输入，且没有提交内容存在浏览器的存储中，这样navigate away的时候，用户再回来的时候，还能看到之前的输入
