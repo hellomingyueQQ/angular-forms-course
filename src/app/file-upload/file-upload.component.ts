4f54128ba3c612bac91010c961206a8ca769e3a5
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-08 20:25:57 +0800

    update

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,16 +1,21 @@
-import {Component, Input} from '@angular/core';
-import { HttpClient, HttpEventType } from '@angular/common/http';
-import {catchError, finalize} from 'rxjs/operators';
-import {AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator} from '@angular/forms';
-import {noop, of} from 'rxjs';
-
+import { Component, Input } from "@angular/core";
+import { HttpClient, HttpEventType } from "@angular/common/http";
+import { catchError, finalize } from "rxjs/operators";
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  Validator,
+} from "@angular/forms";
+import { noop, of } from "rxjs";
 
 @Component({
-  selector: 'file-upload',
+  selector: "file-upload",
   templateUrl: "file-upload.component.html",
-  styleUrls: ["file-upload.component.scss"]
+  styleUrls: ["file-upload.component.scss"],
 })
 export class FileUploadComponent {
-
-
+  @Input()
+  requiredFileType: string;
 }
