c2f2ad23a4575a78917ae0f3285bac9258c1e286
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-08 19:44:10 +0800

    update

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,26 +1,32 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-
+import { createPromoRangeValidator } from "../../validators/date-range.validator";
 @Component({
   selector: "create-course-step-2",
   templateUrl: "create-course-step-2.component.html",
   styleUrls: ["create-course-step-2.component.scss"],
 })
 export class CreateCourseStep2Component implements OnInit {
-  form = this.fb.group({
-    courseType: ["premium", Validators.required],
-    price: [
-      null,
-      [
-        Validators.required,
-        Validators.min(1),
-        Validators.max(999),
-        Validators.pattern("[0-9]+"),
+  form = this.fb.group(
+    {
+      courseType: ["premium", Validators.required],
+      price: [
+        null,
+        [
+          Validators.required,
+          Validators.min(1),
+          Validators.max(999),
+          Validators.pattern("[0-9]+"),
+        ],
       ],
-    ],
-    promoStartAt: [null],
-    promoEndAt: [null],
-  });
+      promoStartAt: [null],
+      promoEndAt: [null],
+    },
+    {
+      validators: [createPromoRangeValidator()],
+      updateOn: "blur",
+    }
+  );
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
