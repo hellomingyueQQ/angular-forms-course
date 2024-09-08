a1250521ad9e539eeb15a7a6204dace49dd637b5
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-08 14:26:17 +0800

    Update login-reactive.component.ts

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -13,18 +13,6 @@ import { createPasswordStrengthValidator } from "../validators/password-strength
   styleUrls: ["./login-reactive.component.css"],
 })
 export class LoginReactiveComponent implements OnInit {
-  // email = new FormControl("", {
-  //   validators: [Validators.required, Validators.email],
-  //   updateOn: "blur",
-  // });
-  // password = new FormControl("", {
-  //   validators: [
-  //     Validators.required,
-  //     Validators.minLength(8),
-  //     createPasswordStrengthValidator(),
-  //   ],
-  // });
-
   form = this.fb.group({
     email: [
       "",
@@ -40,26 +28,18 @@ export class LoginReactiveComponent implements OnInit {
         Validators.minLength(8),
         createPasswordStrengthValidator(),
       ],
-      [],
     ],
   });
-  // 第三个数组是异步的validators，这里没有，所以是空数组
-  // ctr = this.fb.control(""); // fb也有创建control的方法, 但是不常用
 
-  // 为了减少verbose，引入formbuilder
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {}
 
   get email() {
-    return this.form.controls["email"]; // this.form.get('email') 根据提示，这个也可以用
+    return this.form.controls["email"];
   }
 
   get password() {
     return this.form.controls["password"];
   }
 }
-// 相比于template，reactive form的html只有formGroup和formControlName两个指令
-// formModel can be dynamic programmatically
-// validator可以使validator function,不需要在依赖注入中配置
-// 作者的观点，如果新项目，尽量用reactive form
