cdd7b0c3d92d8daead660a1c417fd666e9fa66c4
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-09 09:33:47 +0800

    update

--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -1,13 +1,13 @@
-import {Component} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { Component } from "@angular/core";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'create-course-step-3',
-  templateUrl: 'create-course-step-3.component.html',
-  styleUrls: ['create-course-step-3.component.scss']
+  selector: "create-course-step-3",
+  templateUrl: "create-course-step-3.component.html",
+  styleUrls: ["create-course-step-3.component.scss"],
 })
 export class CreateCourseStep3Component {
+  form = this.fb.group({});
 
-
+  constructor(private fb: FormBuilder) {}
 }
