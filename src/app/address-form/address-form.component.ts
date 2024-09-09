32e43b35fbd70b958d2c358ef5d2a6410afcbd99
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-09 08:57:10 +0800

    Update address-form.component.ts

--- src/app/address-form/address-form.component.ts
+++ src/app/address-form/address-form.component.ts
@@ -1,36 +1,43 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import {
   AbstractControl,
   ControlValueAccessor,
-  FormBuilder, FormGroup,
+  FormBuilder,
+  FormGroup,
   NG_VALIDATORS,
   NG_VALUE_ACCESSOR,
   Validator,
-  Validators
-} from '@angular/forms';
-import {noop, Subscription} from 'rxjs';
+  Validators,
+} from "@angular/forms";
+import { noop, Subscription } from "rxjs";
 
 @Component({
-  selector: 'address-form',
-  templateUrl: './address-form.component.html',
-  styleUrls: ['./address-form.component.scss']
+  selector: "address-form",
+  templateUrl: "./address-form.component.html",
+  styleUrls: ["./address-form.component.scss"],
 })
-export class AddressFormComponent {
+export class AddressFormComponent implements ControlValueAccessor {
+  @Input()
+  legend: string;
 
-    @Input()
-    legend:string;
+  form: FormGroup = this.fb.group({
+    addressLine1: [null, [Validators.required]],
+    addressLine2: [null, [Validators.required]],
+    zipCode: [null, [Validators.required]],
+    city: [null, [Validators.required]],
+  });
 
-    form: FormGroup = this.fb.group({
-        addressLine1: [null, [Validators.required]],
-        addressLine2: [null, [Validators.required]],
-        zipCode: [null, [Validators.required]],
-        city: [null, [Validators.required]]
-    });
-
-    constructor(private fb: FormBuilder) {
-    }
-
+  constructor(private fb: FormBuilder) {}
+  writeValue(obj: any): void {
+    throw new Error("Method not implemented.");
+  }
+  registerOnChange(fn: any): void {
+    throw new Error("Method not implemented.");
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error("Method not implemented.");
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error("Method not implemented.");
+  }
 }
-
-
-
